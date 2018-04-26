439d7f87ec2b54b04f3b477e2fb32db46ab01efc
root <root@station104.example.com>
2018-04-26 17:05:02 +0530

    to ceck path is file or dir

---
+++ file_dir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+
+import os  
+path="/etc"  
+if os.path.isdir(path):  
+    print("\nIt is a directory")  
+elif os.path.isfile(path):  
+    print("\nIt is a normal file")  
+else:  
+    print("It is a special file (socket, FIFO, device file)" )
+print()
+
+
+
+
